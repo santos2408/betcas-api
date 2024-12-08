79db2d9eb2be382bd29cee9887bd270520eca76e
Roger Santos <roger.santos36@gmail.com>
2024-12-07 21:32:45 -0300

    chore: ajustando env

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 const BASE_API_FOOTBALL = process.env.API_FOOTBALL_URL;
+const API_KEY_FOOTBALL = process.env.API_KEY_FOOTBALL;
 
 const http_api_football = axios.create({
   baseURL: BASE_API_FOOTBALL,
   headers: {
-    Authorization: "Bearer live_40d63df9ff115f28ea51c345300522", // guardar api key no .env
-    // test_c2d93f7d290d175539e3ea78a358aa
-    // live_40d63df9ff115f28ea51c345300522
+    Authorization: `Bearer ${API_KEY_FOOTBALL}`, // guardar api key no .env
   },
 });
 
