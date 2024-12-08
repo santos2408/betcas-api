631d59c85f4d8b7bf250d3ca1b87b2c547ecb867
Roger Santos <roger.santos36@gmail.com>
2024-12-08 00:16:55 -0300

    chore: adicionando e atualizando apenas os campeonatos disponíveis no plano da API

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const BASE_API_FOOTBALL = process.env.API_FOOTBALL_URL;
-const API_KEY_FOOTBALL = process.env.API_KEY_FOOTBALL;
+const API_KEY_FOOTBALL_LIVE = process.env.API_KEY_FOOTBALL_LIVE;
+// const API_KEY_FOOTBALL_TEST = process.env.API_KEY_FOOTBALL_TEST;
 
 const http_api_football = axios.create({
   baseURL: BASE_API_FOOTBALL,
   headers: {
-    Authorization: `Bearer ${API_KEY_FOOTBALL}`, // guardar api key no .env
+    Authorization: `Bearer ${API_KEY_FOOTBALL_LIVE}`, // guardar api key no .env
   },
 });
 
